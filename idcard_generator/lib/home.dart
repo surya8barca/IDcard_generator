2aea2c8e45ac404bcf37b6b834808b133c872219
surya8barca <surya8barca@gmail.com>
2020-05-06 09:18:28 +0530

    setup app

---
+++ idcard_generator/lib/home.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+class Home extends StatefulWidget {
+  Home({Key key}) : super(key: key);
+
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.blueGrey,
+        centerTitle: true,
+        title: Text(
+          'ID Card Generator',
+          style: TextStyle(
+            fontSize: 28.0,
+            color: Colors.black,
+          ),
+        ),
+      ),
+    );
+  }
+}
